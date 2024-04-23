8e50f8a22265919a0312280aae14bbc0d9293890
KyhleYase <kyhleyase.23@ichat.sp.edu.sg>
2024-04-23 16:43:30 +0800

    THIS IS TRASH

---
+++ test.py
@@ -0,0 +1,13 @@
+def display_main_menu():
+    print("Enter some numbers SEPARATED BY COMMAS (e.g. 5, 67, 32")
+    
+display_main_menu()
+
+
+def calc_average(numlist1):
+    print("This is your average temperature: ")
+    total_temp = sum(numlist1)
+    average_temp = total_temp / len(numlist1)
+    return average_temp
+
+#this is just a code dump
