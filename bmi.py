429f7dc1dbf44768d2147edac076b717c7a232b3
KyhleYase <kyhleyase.23@ichat.sp.edu.sg>
2024-04-30 13:09:01 +0800

    Tests bmi

--- bmi.py
+++ bmi.py
@@ -6,10 +6,13 @@ def calculate_bmi(height, weight):
     print("BMI = " + str(bmi))
     if bmi<18.5 :
         print("Under weight")
+        return -1
     elif 18.5<=bmi<25.0 :
         print("Normal weight")
+        return 0
     else :  
         print("Overweight")
+        return 1
      
     
 calculate_bmi(weight=74, height=1.73)
--- test.py
+++ test.py
@@ -1,13 +1,5 @@
-def display_main_menu():
-    print("Enter some numbers SEPARATED BY COMMAS (e.g. 5, 67, 32")
-    
-display_main_menu()
 
-
-def calc_average(numlist1):
-    print("This is your average temperature: ")
-    total_temp = sum(numlist1)
-    average_temp = total_temp / len(numlist1)
-    return average_temp
-
-#this is just a code dump
+def calculate_bmi(height, weight):
+    print("Height = " + height)
+    print("Weight = " + weight)
+calculate_bmi(weight=57, height=1.73)
